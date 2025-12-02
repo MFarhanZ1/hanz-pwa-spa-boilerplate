ffbbdcde7200294fe803913339db776d484b2d16
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-12-02 16:50:25 +0700

    add better auth integration

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,16 @@ import { QueryClientProvider } from "@tanstack/react-query";
 
 export default function App() {
     return (
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-          <RouterProvider router={router} />
-          <Toaster
-                position="bottom-right" 
-                richColors 
-                closeButton 
-                expand
-              />
-        </ThemeProvider>
-      </QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+            <RouterProvider router={router} />
+            <Toaster
+                  position="bottom-right" 
+                  richColors 
+                  closeButton 
+                  expand
+                />
+          </ThemeProvider>
+        </QueryClientProvider>
     )
 }
---
+++ src/pages/auth/AuthPage.tsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom"
+
+export default function AuthPage() {
+
+  return (
+    <main className="container mx-auto flex grow flex-col items-center justify-center gap-3 self-center p-4 md:p-6">
+    </main>
+  )
+}
