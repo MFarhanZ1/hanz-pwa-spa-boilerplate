7430b1bd95894d30e8d7d693f114586b0351e2e6
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-12-02 16:17:00 +0700

    adding init pwa conf

--- src/pages/publics/landing.page.tsx
+++ src/pages/publics/landing.page.tsx
@@ -1,7 +1,7 @@
 export default function LandingPage() {
     return (
         <div>
-            <h1>Landing Page</h1>
+            <h1 className="text-3xl font-bold">Landing Page</h1>
         </div>
     )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,42 @@ import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA, type VitePWAOptions } from 'vite-plugin-pwa'
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  includeAssets: ['favicon.ico', "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "SaKu Driver",
+    short_name: "SaKu Driver",
+    description: "Layanan ojek online dari mahasiswa, oleh mahasiswa, untuk mahasiswa.",
+    icons: [
+    {
+      src: '/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'favicon'
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any maskable',
+    }
+    ],
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: "standalone",
+    scope: '/',
+    start_url: "/",
+    orientation: 'portrait'
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-    resolve: {
+  plugins: [react(), tailwindcss(), VitePWA(manifestForPlugIn)],
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
